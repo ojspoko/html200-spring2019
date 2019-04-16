8cd5f050c2a62f5c4d73b5858df243fa899653da
ojspoko <ojspoko@gmail.com>
2019-04-15 21:06:36 -0700

    updated file name

---
+++ Lesson02/calculator.js
@@ -0,0 +1,17 @@
+const x = 4;
+
+console.log(x * 2);
+
+const y = 42;
+
+console.log(y > 8);
+
+let i = 5;
+let o = '5';
+
+console.log((i-3) === o);
+
+const f = 80;
+const u = f % 2;
+
+console.log((f+4) - u);
