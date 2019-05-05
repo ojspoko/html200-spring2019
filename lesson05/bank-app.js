67d26596b9315196440c718f949a623216319bb5
ojspoko <ojspoko@gmail.com>
2019-05-05 19:27:23 -0400

    changed from if/else to switch case after watching office hours

--- lesson05/bank-app.js
+++ lesson05/bank-app.js
@@ -4,17 +4,25 @@ let balance = 500;
 while (quit === false) {
   let input = prompt('Select an action');
 
-  if (input === 'Q'|| input === 'q') {
-      quit = true;
-  } else if (input === 'W'|| input === 'w') {
-      let withdrawAmount = prompt('Enter an amount to withdraw');
-      balance = balance - Number(withdrawAmount);
-      alert('Balance: $' + balance);
-  } else if (input === 'D'|| input === 'd') {
-      let depositAmount = prompt('Enter an amount to deposit');
-      balance = balance + Number(depositAmount);
-      alert('Balance: $' + balance);
-  } else if (input === 'B'|| input === 'b') {
-      alert('Balance: $' + balance);
+  switch(input) {
+    case 'Q':
+    quit = true;
+    break;
+
+    case 'B':
+    alert('Balance: $' + balance);
+    break;
+
+    case 'W':
+    let withdrawAmount = prompt('Enter an amount to withdraw');
+    balance = balance - Number(withdrawAmount);
+    alert('Balance: $' + balance);
+    break;
+
+    case 'D':
+    let depositAmount = prompt('Enter an amount to deposit');
+    balance = balance + Number(depositAmount);
+    alert('Balance: $' + balance);
+    break;
   }
 }
