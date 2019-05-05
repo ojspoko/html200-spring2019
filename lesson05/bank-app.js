77adda2647432d835d7a12504496cff61b613e8a
ojspoko <ojspoko@gmail.com>
2019-05-05 19:13:51 -0400

    simplified code

--- lesson05/bank-app.js
+++ lesson05/bank-app.js
@@ -2,19 +2,19 @@ let quit = false;
 let balance = 500;
 
 while (quit === false) {
-  let input = prompt('Select an action')
+  let input = prompt('Select an action');
 
-  if (input === 'q') {
+  if (input === 'Q'|| input === 'q') {
       quit = true;
-  } else if (input === 'w') {
+  } else if (input === 'W'|| input === 'w') {
       let withdrawAmount = prompt('Enter an amount to withdraw');
       balance = balance - Number(withdrawAmount);
-      alert(balance);
-  } else if (input === 'd') {
+      alert('Balance: $' + balance);
+  } else if (input === 'D'|| input === 'd') {
       let depositAmount = prompt('Enter an amount to deposit');
       balance = balance + Number(depositAmount);
-      alert(balance);
-  } else if (input === 'b') {
-      alert(balance);
+      alert('Balance: $' + balance);
+  } else if (input === 'B'|| input === 'b') {
+      alert('Balance: $' + balance);
   }
 }
